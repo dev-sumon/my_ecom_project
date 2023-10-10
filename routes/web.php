7a76c815137057dedaf22b6ccdd24ce57cf597c9
dev-sumon <akhtaruzzamansumon7@gmail.com>
2023-10-10 20:50:16 +0600

    change

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great! kjgrlifor;frfligflfligvokli
+| be assigned to the "web" middleware group. Make something great! 
 |
 */
 
